536ee65d4ab93a0d25eed83664aa38b5a062f1ff
Patryk Jaworski <regalis@regalis.com.pl>
2014-01-10 13:46:37 +0100

    Add authentication functions, add users db class
    * add User class with import/export functionalities
    * add UsersDatabase class for saving/restoring User data (closes #9)

--- src/server.js
+++ src/server.js
@@ -26,6 +26,26 @@ var path = require("path");
 var url = require("url");
 var fs = require("fs");
 
+/*** Clients block ***/
+
+var clients = {};
+
+var clients_register = function(client_id) {
+	clients[user_id] = false;
+}
+
+var clients_authenticate = function(client_id, user_id) {
+	clients[client_id] = user_id;
+}
+
+var clients_exists = function(client_id) {
+	return (client_id in clients);
+}
+
+var clients_is_authenticated = function(client_id) {
+	return (clients_exists(client_id) && clients[client_id] != false);
+}
+
 String.prototype.startsWith = function(prefix) {
     return this.indexOf(prefix) === 0;
 }
@@ -33,6 +53,77 @@ String.prototype.startsWith = function(prefix) {
 String.prototype.endsWith = function(suffix) {
     return this.match(suffix+"$") == suffix;
 };
+
+/*** Users block ***/
+
+function User() {
+	this.id = null;
+	this._login = null;
+	this._first_name = null;
+	this._last_name = null;
+	this._password = null;
+	this._email = null;
+	this._friends_list = [];
+	this._requests_list = [];
+	this._avatar = null;
+	this.availability = null;
+
+	this.export_to_json = function() {
+		var json = {};
+		for (x in this)  {
+			if (x.startsWith('_'))
+				json[x.substring(1)] = this[x];
+		}
+		return JSON.stringify(json);
+	}
+}
+
+var new_user_from_json = function(json) {
+	var obj = JSON.parse(json);
+	var user = new User();
+	for (x in obj) {
+		user['_' + x] = obj[x];
+	}
+	return user;
+}
+
+/*** Database block ***/
+
+/** Class for manipulating users database
+ *
+ * Used directories:
+ *   -> var/users/
+ * Used files:
+ *   -> var/users/$ID
+ *   -> var/users/logins
+ */
+function UsersDatabase() {
+	var db_path = path.join(process.cwd(), "/var/users");
+	var validate_user = function(user) {
+		// TODO: perform full validation
+		if (isNaN(user.id))
+			throw "Bad user id";
+	}
+
+	this.save_user_data = function(user) {
+		validate_user(user);
+		fs.writeFileSync(path.join(db_path, user.id.toString()), user.export_to_json(), 'utf8', function(err) {
+			if (err)
+				throw err;
+		});
+		return true;
+	}
+	
+	this.read_user_data = function(user_id) {
+		var json = fs.readFileSync(path.join(db_path, user_id.toString()), 'utf8');
+		if (!json)
+			throw "Unable to read user data...";
+		if (Buffer.isBuffer(json))
+			json = json.toString('utf8');
+		return new_user_from_json(json);
+	}
+
+}
 
 var http_server = http.createServer(function(request, response) {
 	var requested_path = url.parse(request.url).pathname;
@@ -66,12 +157,14 @@ var http_server = http.createServer(function(request, response) {
 var io = require("socket.io").listen(http_server);
 
 io.sockets.on("connection", function(socket) {
-	console.info("Got new WebSocket connection...");
+	console.info("Got new WebSocket connection (" + socket.id + ")...");
+	clients_register(socket.id);
 	socket.on('ping', function(data) {
 		console.log("Got ping command from client...");
 		socket.emit("pong", data);
 		console.log("Sending pong...");
 	});
+	
 });
 
 http_server.listen(9393);
