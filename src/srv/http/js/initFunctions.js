592c965a074199b09be5f2ee8c200dd970632f0a
alek gajos <alek.gajos@gmail.com>
2014-01-20 22:57:48 +0100

    Added file initFunctions.js

---
+++ src/srv/http/js/initFunctions.js
@@ -0,0 +1,41 @@
+/*
+ *
+ * Copyright (C) Jamii Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *  -> Alek Gajos <alek.gajos@gmail.com>
+ *
+ */
+
+/** 
+ *  Init function creating necessaty objects and registering handlers for packets.
+ *  To be called after successful user login.
+ *
+*/
+function initMainScreen(){
+    
+    // inside FriendListGUI constructor, friendList is created as flg.fl
+    flg = new FriendListGUI("fList");
+    window.flg = flg;
+
+    // register handlers
+    window.connection.registerHandler("friendsData", flg.fl.gotFriendsDataHandler);
+
+    // request friend contacts from server
+    flg.fl.populateList();
+    
+    
+}
