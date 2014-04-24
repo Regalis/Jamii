bbb37a6939e93d860c169e53224d2c289e42ec42
Patryk Jaworski <regalis@regalis.com.pl>
2014-04-24 23:02:55 +0200

    Fix password change

--- src/srv/http/js/connectionManager.js
+++ src/srv/http/js/connectionManager.js
@@ -97,7 +97,6 @@ ConnectionManager.prototype.getValue = function( cookie, key ){
  *
  */
 ConnectionManager.prototype.send = function( header, data ){
-
     this.counter++;
 
     var cargo = {};
@@ -106,7 +105,6 @@ ConnectionManager.prototype.send = function( header, data ){
     cargo['data'] = data;
     
     this.socket.emit( header, cargo );
-
 }
 
 /**
