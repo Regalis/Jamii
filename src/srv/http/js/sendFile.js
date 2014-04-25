54fabd66de2778f2ba951cb20f3a6c29a35d69a7
Mateusz Zajac <matteo.zajac@gmail.com>
2014-04-25 17:59:48 +0200

    whiteboard picker and cleaner

--- src/srv/http/js/sendFile.js
+++ src/srv/http/js/sendFile.js
@@ -2,7 +2,7 @@ function file_transfer(){
 
 var data = {};
 data["file"]= window.file;
-data["name"]=window.file_name;
+data["name"]= window.file_name;
 window.connection.send("send_file", data);
 
 return false;
