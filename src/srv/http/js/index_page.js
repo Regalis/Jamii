bc791223a4e6bf30caaaaaa510560c950b55d094
Mateusz Zajac <matteo.zajac@gmail.com>
2014-05-15 13:58:17 +0200

    Changed index design

---
+++ src/srv/http/js/index_page.js
@@ -0,0 +1,72 @@
+var socket = null;
+/*
+function log(data) {
+    document.getElementById("status").textContent += data + "\n";
+}
+*/
+window.onload = function () {
+    /*
+     * TODO: Duplication!
+     * Source: srv/http/js/initFunctions.js:31
+     */
+    var host = window.location.host;
+    if (host.indexOf(':') != -1) {
+        host = host.substring(0, host.indexOf(':'));
+    }
+    window.connection = new ConnectionManager("http://" + host, "9393");
+
+    document.getElementsByClassName("login_button")[0].onclick = function () {
+        log("Get login data");
+        var data = {};
+
+        data["login"] = document.getElementById("login").value;
+        data["passwd"] = document.getElementById("passwd").value;
+        window.connection.send("login", data);
+        <!-- window.connection.registerHandler("loginOK", function(data){ -->
+        <!-- window.location.href = "/page.html"; -->
+        <!-- }); -->
+        window.connection.registerHandler("loginBAD", function (data) {
+            alert(data.what);
+        });
+
+
+        return false;
+    }
+    document.getElementsByClassName("register_button")[0].onclick = function () {
+        log("Get register data");
+        var data = {};
+
+        data["login"] = document.getElementById("loginReg").value;
+
+        data["first_name"] = document.getElementById("first_name").value;
+        data["last_name"] = document.getElementById("last_name").value;
+        data["email"] = document.getElementById("email").value;
+        data["passwd"] = document.getElementById("passwdReg").value;
+
+        window.connection.send("register", data);
+
+        window.connection.registerHandler("registerOK", function (data) {
+         alert("Registration succesfull");
+			document.getElementById("registration_form").style.display="none";
+			document.getElementById("login_form").style.display="block";
+        });
+        return false;
+    }
+
+}
+
+
+function register_pop(){
+	if(		document.getElementById("swap").innerHTML=="Login"){
+				document.getElementById("swap").innerHTML="Register";
+	document.getElementById("registration_form").style.display="none";
+	document.getElementById("login_form").style.display="block";
+	}
+else{
+	document.getElementById("swap").innerHTML="Login";
+	document.getElementById("registration_form").style.display="block";
+	document.getElementById("login_form").style.display="none";
+}
+
+
+}
